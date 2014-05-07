306a66af23f3bc0079879b00c7f55f2ac36d10c5
frankJXL <frankjxl@icloud.com>
2014-05-07 17:54:05 -0400

    Added multiple views and controllers
    Implemented multiple views (init.ejs) and their styling (.less)
    Added rendering code in d3px.js

--- d3px/d3px.js
+++ d3px/d3px.js
@@ -1,9 +1,21 @@
 steal(
 	'can',
 	'./views/nav/nav.ejs',
+    './controls/nav-search/init.ejs',
+    './views/toolbar/toolbar.ejs',
+    './controls/profile-heroes/init.ejs',
+    './controls/profile-career/init.ejs',
+    './d3px.less',
     './views/nav/nav.less',
-	'./d3px.less',
+    './controls/nav-search/init.less',
+    './views/toolbar/toolbar.less',
+    './controls/profile-heroes/init.less',
+    './controls/profile-career/init.less',
     './models/fixtures/fixtures.js',
-function(can, navView){
-    $('body').html(navView({battletag: 'Parallax#1878', catchphrase: 'Stay a while and listen'}));
+function(can, navView, navSearch, toolbar, profileHeroes, profileCareer){
+	$('#canvas').append(profileHeroes());
+	$('#canvas').append(profileCareer());
+    $('body').append(navView({battletag: 'Parallax#1878', catchphrase: 'Stay a while and listen'}));
+    $('#navbar #search-module').html(navSearch());
+    $('body').append(toolbar());
 });
--- d3px/index.php
+++ d3px/index.php
@@ -4,11 +4,7 @@
 		<title>Diablo 3: Profile X</title>
 	</head>
 	<body>
-        <h1>Diablo 3: Profile X</h1>
-        
-        <!--<div id="searchbar"></div>
-        <div id="playerProfile"></div>-->
-
         <script type='text/javascript' src='../steal/steal.js?d3px'></script>
+        <div id="canvas"></div>
     </body>
 </html>
