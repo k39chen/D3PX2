be2e297f8fb586082d1079676311c2df3be64031
frankJXL <frankjxl@icloud.com>
2014-05-07 21:16:13 -0400

    Implemented profile heroes view

---
+++ d3px/controls/profile-heroes/profile-heroes.js
@@ -0,0 +1,26 @@
+steal(
+	'can',
+	'../../models/d3api.js',
+	'./init.ejs',
+	'./init.less', function(can, D3API, heroesView) {
+		
+		return can.Control({
+			defaults: {}
+		},
+		{
+			init: function() {
+				// Get list of heroes from D3API
+				console.log("inside");
+				var battleTag = 'gummypower-1650';
+
+			    D3API.getPlayerProfile({battleTag:battleTag}).done(function(data){
+			        // here is the data:
+			        console.log(data);
+			    });
+			    
+			    // Insert into HTML
+			    $('#canvas').append(heroesView);
+			}
+		});
+		
+	});
--- d3px/d3px.js
+++ d3px/d3px.js
@@ -1,34 +1,19 @@
 steal(
 	'can',
-    './models/d3api.js',
 	'./views/nav/nav.ejs',
     './controls/nav-search/init.ejs',
     './views/toolbar/toolbar.ejs',
-    './controls/profile-heroes/init.ejs',
+    './controls/profile-heroes/profile-heroes.js',
     './controls/profile-career/init.ejs',
     './d3px.less',
     './views/nav/nav.less',
     './controls/nav-search/init.less',
     './views/toolbar/toolbar.less',
-    './controls/profile-heroes/init.less',
     './controls/profile-career/init.less',
-    './models/fixtures/fixtures.js',
-function(can, D3API, navView, navSearch, toolbar, profileHeroes, profileCareer){
-
-    var battleTag = 'gummypower-1650';
-
-
-    D3API.getPlayerProfile({battleTag:battleTag}).done(function(data){
-        
-        // here is the data:
-        console.log(data);
-
-        // it is returned as a CanJS model, so if you want to iterate over the attributes then use: [___]._data
-        // ...
-
-    });
-
-	$('#canvas').append(profileHeroes());
+function(can, navView, navSearch, toolbar, profileHeroes, profileCareer){
+	
+	var profileHeroes = new profileHeroes();
+	
 	$('#canvas').append(profileCareer());
     $('body').append(navView({battletag: 'Parallax#1878', catchphrase: 'Stay a while and listen'}));
     $('#navbar #search-module').html(navSearch());
