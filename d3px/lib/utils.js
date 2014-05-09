d1998eb3f72ceb56de22a4944f9917ac30944514
Kevin Chen <k39chen@gmail.com>
2014-05-08 21:52:31 -0400

    Extended formatNumber method

--- d3px/lib/utils.js
+++ d3px/lib/utils.js
@@ -8,6 +8,7 @@
  *   - commas {Boolean} Flag to show commas. [default=true]
  *   - prefix {String} An optional prefix. [default='']
  *   - postfix {String} An optional postfix. [default='']
+ *   - operation {Function} An optional operation. [default=null]
  * @return {String} The formatted number
  */
 function formatNumber(num, options) {
@@ -15,9 +16,14 @@ function formatNumber(num, options) {
         precision: 0,
         commas: true,
         prefix: '',
-        postfix: ''
+        postfix: '',
+        operation: null
     }, options);
 
+    // perform a custom operation
+    if (o.operation) {
+        num = o.operation(num);
+    }
     // apply the precision
     num = num.toFixed(o.precision);
 
@@ -25,10 +31,13 @@ function formatNumber(num, options) {
     if (o.commas) {
         var parts = num.toString().split(".");
         parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        return parts.join(".");
+        num = parts.join(".");
     }
+    parts = num.split(',');
+    num = (parts.length <= 2) ? parts.join('') : parts.join(',');
+
     // add pre/postfix
-    return o.prefix+num+o.postfix;
+    return o['prefix']+num+o.postfix;
 }
 /**
  * Reports the number of properties in object
